e4c8fe1533e7c3ce166ad1dd548ee93b51289555
gustavoresque <gustavo.cbcc@gmail.com>
2023-09-13 17:39:39 -0300

    testeconflito

--- src/meubrasfoot/MeuBrasfootMain.java
+++ src/meubrasfoot/MeuBrasfootMain.java
@@ -22,11 +22,11 @@ public class MeuBrasfootMain {
         Clube paysandu = new Clube("Paysandu", "PAY", "Curuzu", "Pará", 1914);
         Clube tuna = new Clube("Tuna", "TUN", "Estadio", "Pará", 1930);
         
-        Jogador j = new Jogador("Gustavo0",1,"GK",200,paysandu,30);
-        Jogador j1 = new Jogador("Gustavo1",2,"CB",200,paysandu,30);
-        Jogador j2 = new Jogador("Gustavo2",3,"CB",200,paysandu,30);
-        Jogador j3 = new Jogador("Gustavo3",4,"CM",200,paysandu,30);
-        Jogador j4 = new Jogador("Gustavo4",5,"ST",200,paysandu,30);
+        Jogador j = new Jogador("Gustavo0 ",1,"GK",200,paysandu,30);
+        Jogador j1 = new Jogador("Gustavo1 ",2,"CB",200,paysandu,30);
+        Jogador j2 = new Jogador("Gustavo2 ",3,"CB",200,paysandu,30);
+        Jogador j3 = new Jogador("Gustavo3 ",4,"CM",200,paysandu,30);
+        Jogador j4 = new Jogador("Gustavo4 ",5,"ST",200,paysandu,30);
         
         Jogador j5 = new Jogador("Gustavo5",1,"GK",200,tuna,30);
         Jogador j6 = new Jogador("Gustavo6",5,"CB",200,tuna,30);
